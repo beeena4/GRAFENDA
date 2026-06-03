5e32d75f848dcbf36d77a6fb921733646fda84e0
beeena4 <febridkune54@gmail.com>
2026-06-03 14:54:05 +0700

    add

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,8 @@ const corsOptions = {
       'http://127.0.0.1:5173',
       'http://127.0.0.1:5174',
       'http://127.0.0.1:5175',
-      'http://127.0.0.1:5176'
+      'http://127.0.0.1:5176',
+      'https://grafenda.vercel.app'
     ].filter(Boolean);
 
     if (!origin || allowedOrigins.includes(origin)) {
